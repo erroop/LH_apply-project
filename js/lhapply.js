988de00dd5ebb1e7d75041e2875255097afd7a3e
erroop <erroop0@gmail.com>
2024-02-29 21:01:33 +0900

    desktop third commit update lhapply

--- js/lhapply.js
+++ js/lhapply.js
@@ -15,6 +15,7 @@ $(function(){
 
     // MenuApplyItem click =========================
     const MainApplyList_Item = $(".MainApplyList li")
+    const MainApplyImgContainer = $(".MainApplyImgContainer .MainApplyImg-inner")
     
     //first commit--------
     MainApplyList_Item.eq(0).addClass("on")
@@ -26,7 +27,51 @@ $(function(){
         $(this).addClass("on")
         MainApplyList_Item.find("a").removeClass("on")
        $(this).find("a").addClass("on")
+       if($(this).parent().children()[0].children[0].className === "MainApplyItem-link on"){
+        MainApplyImgContainer.css({display : "none"})
+        MainApplyImgContainer.eq(0).css({display:"block"})
+       }
+       else if($(this).parent().children()[1].children[0].className === "MainApplyItem-link on"){
+        MainApplyImgContainer.css({display : "none"})
+        MainApplyImgContainer.eq(1).css({display:"block"})
+       }else if($(this).parent().children()[2].children[0].className === "MainApplyItem-link on"){
+        MainApplyImgContainer.css({display : "none"})
+        MainApplyImgContainer.eq(2).css({display:"block"})
+       }else if($(this).parent().children()[3].children[0].className === "MainApplyItem-link on"){
+        MainApplyImgContainer.css({display : "none"})
+        MainApplyImgContainer.eq(3).css({display:"block"})
+       }else if($(this).parent().children()[4].children[0].className === "MainApplyItem-link on"){
+        MainApplyImgContainer.css({display : "none"})
+        MainApplyImgContainer.eq(4).css({display:"block"})
+       }
     })
+
+    //Notice title click ================================
+    const Title_list = $(".Notice-title-list li");
+    const info_title = $(".Notice-title-info a");
+    const menu_list = $(".Notice-menu .Notice-menu-list-container");
+
+    console.log(info_title.text())
+    //first commit---------------------------
+    Title_list.eq(0).addClass("on")
+
+    Title_list.click(function(){
+        Title_list.removeClass("on")
+        $(this).addClass("on")
+
+        if($(this).text() === "공고문"){
+            info_title.html("전체보기 <span></span>")
+            menu_list.eq(0).css({display : "block"})
+            menu_list.eq(1).css({display : "none"})
+        }
+        else if($(this).text() === "공지사항"){
+            info_title.html("더보기 <span></span>")
+            menu_list.eq(0).css({display : "none"})
+            menu_list.eq(1).css({display : "block"})
+        }
+    })
+
+
 })
 
 
